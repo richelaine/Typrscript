f91b16f921ddbaf1e94fb9704fe994a43340d8be
Regine Dionisio <dionisio.regine26@gmail.com>
2017-06-06 15:25:09 +0800

    methods sample

--- basics.ts
+++ basics.ts
@@ -1,11 +1,28 @@
-class Person
+// -- METHODS Sample --
+// Methods - functions that run in context of an object.
+
+class Person // Object
 {
     first_name: string;
     last_name: string;
     age: number;
-
-    greet()
+ 
+    greet() // method
     {
         console.log("Hello", this.first_name);
     }
+
+    ageInYears(years: number): number // method
+    {
+        return this.age + years;
+    }
 }
+
+// instantiate a new Person instance
+var p: Person = new Person(); 
+
+// set initial age
+p.age = 6;
+
+// how old will he be in 12 years?
+p.ageInYears(12);
